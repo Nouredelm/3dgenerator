98c22cb978ec0c10a19b71176dc12ba4dd8ad2ab
Nouredelm <nimdo2@yahoo.fr>
2026-05-09 19:57:26 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import { defineConfig } from 'vite'
+
 export default defineConfig({
   base: '/3dgenerator/', // Add this line
   // ... remaining config
